1dd76feb4474474e139c8261a4182be25dc601dd
Rogério Ferreira de souza <rogeriofrsouza@gmail.com>
2021-02-24 15:25:20 -0300

    Delete Desafio 6.py

--- Desafio 6.py
+++
@@ -1,7 +0,0 @@
-n = int(input('Digite um número inteiro: '))
-d = n * 2
-t = n * 3
-r = n ** (1/2)
-#r = pow(n, (1/2)
-print(f'O dobro de {n} vale {d}.')
-print(f'O triplo de {n} vale {t}. \nA raiz quadrada de {n} é igual a {r:.2f}')
