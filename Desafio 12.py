2048d53e604b55b2713648592a19947889bd884c
Rogério Ferreira de souza <rogeriofrsouza@gmail.com>
2021-02-24 15:25:01 -0300

    Delete Desafio 12.py

--- Desafio 12.py
+++
@@ -1,4 +0,0 @@
-p = float(input('Digite o preço do produto: R$ '))
-d = p * 0.95
-#d = p - (p * 5/100)
-print(f'O produto que custava R$ {p:.2f}, na promoção com 5% de desconto vai custar: R$ {d:.2f}')
