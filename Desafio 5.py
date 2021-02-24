30b531072175caab3c90cce520321078c8382a8b
Rogério Ferreira de souza <rogeriofrsouza@gmail.com>
2021-02-24 15:25:17 -0300

    Delete Desafio 5.py

--- Desafio 5.py
+++
@@ -1,5 +0,0 @@
-n = int(input('Digite um número inteiro: '))
-a = n - 1
-s = n + 1
-#print(f'Analisando o valor {n}, seu antecessor é {n-1} e o sucessor é {n+1}')
-print(f'Analisando o valor {n}, seu antecessor é {a} e o sucessor é {s}.')
