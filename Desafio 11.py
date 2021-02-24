a540140feb75f37e125ac7d200f859ff17da55ee
Rogério Ferreira de souza <rogeriofrsouza@gmail.com>
2021-02-24 15:19:25 -0300

    Delete Desafio 11.py

--- Desafio 11.py
+++
@@ -1,6 +0,0 @@
-alt = float(input('Altura da parede (m): '))
-larg = float(input('Largura da parede (m): '))
-a = alt * larg
-t = a / 2
-print(f'Sua parede tem a dimensão de {alt:.2f}x{larg:.2f} e sua área é de {a:.2f}m².')
-print(f'Para pintar essa parede, você precisará de {t:.2f} litros (L) de tinta.')
