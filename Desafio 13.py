ecc967c8195539c8bf949874802278cac28edd90
Rogério Ferreira de souza <rogeriofrsouza@gmail.com>
2021-02-24 15:11:46 -0300

    Add files via upload
    Desafios e exercícios desenvolvidos durante o curso de Python 3, no canal do Youtube "Curso em Vídeo", desenvolvido pelo professor Gustavo Guanabara.
    
    I have here some exercises and challenges I made during the Python 3 course, from Gustavo Guanabara's channel on Youtube: "Curso em Vìdeo".

---
+++ Desafio 13.py
@@ -0,0 +1,4 @@
+s = float(input('Qual é o salário do funcionário? R$ '))
+a = s*1.15
+#a = s + (s * 15/100)
+print(f'Um funcionário que ganhava R$ {s:.2f}, com 15% de aumento, passa a receber R$ {a:.2f}')
